92df69a93dd06343b09a238792fec4186f7d8cb5
BugHunter-X <askzenai@gmail.com>
2025-02-15 12:28:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,29 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+    return () => {
+      setIsMounted(false);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isMounted) {
+      console.log('Count:', count);
+    }
+    return () => {
+      console.log('Cleanup!');
+    };
+  }, [count, isMounted]);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+```
